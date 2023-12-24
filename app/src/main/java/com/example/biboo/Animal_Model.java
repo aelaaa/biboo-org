8a94470f82af1b5b983b89f681c8ae37c0c99f1f
YUNIKKAI <mabuchiiii27@gmail.com>
2023-12-24 10:42:52 +0800

    Gallery Update as of 12/24/2023

---
+++ app/src/main/java/com/example/biboo/Animal_Model.java
@@ -0,0 +1,42 @@
+package com.example.biboo;
+
+public class Animal_Model {
+    String animal_name, scientificName, classification, habitat, diet, description, img_source;
+    int id;
+
+    public Animal_Model(int id, String animal, String scientific_name, String classification, String habitat, String diet, String description, String image_source) {
+        this.id = id;
+        this.animal_name = animal;
+        this.scientificName = scientific_name;
+        this.classification = classification;
+        this.habitat = habitat;
+        this.diet = diet;
+        this.description = description;
+        this.img_source = image_source;
+    }
+        //get and set method for setting and getting data retrieval
+        public int getId() {return id;}
+        public void setId() {this.id = id;}
+        public String getAnimal_Name() {
+            return animal_name;
+        }
+        public void setAnimal_Name(String animal) {this.animal_name = animal;}
+        public String getScientificName() {return scientificName;}
+        public void setScientificName(String scientificName) {this.scientificName = scientificName;}
+        public String getClassification() {return classification;}
+        public void setClassification(String classification) {this.classification = classification;}
+        public String getHabitat(){return habitat;}
+        public void setHabitat(String habitat) {this.habitat = habitat;}
+        public String getDiet() {return diet;}
+        public void setDiet(String diet) {this.diet = diet;}
+        public String getDescription() {return description;}
+        public void setDescription(String description) {this.description = description;}
+        public String getImageSource() {
+            return img_source;
+        }
+
+        public void setImageSource(String image_source) {
+            this.img_source = image_source;
+        }
+
+}
